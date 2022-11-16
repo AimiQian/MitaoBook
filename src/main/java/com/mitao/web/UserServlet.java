540c914a209d946bff25ac2ec3bb6de10612fe63
AimiQian <77937184+AimiQian@users.noreply.github.com>
2022-11-16 13:19:11 -0500

    Update UserServlet.java

--- src/main/java/com/mitao/web/UserServlet.java
+++ src/main/java/com/mitao/web/UserServlet.java
@@ -41,6 +41,7 @@ public class UserServlet extends BaseServlet {
 
     protected void logout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             req.getSession().invalidate(); 
+            print(req.getContextPath());
             resp.sendRedirect(req.getContextPath());
     }
 
